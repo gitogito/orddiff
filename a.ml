f986c242849e935e52d6380cbb37991ff13e8ed1
Yoshiki Wada <yoshiki.wada@gmail.com>
2014-05-24 23:47:27 +0900

    Solve 2nd-order ordinary differential equation

--- a.ml
+++ a.ml
@@ -1,27 +1,25 @@
 open Printf
 
-let xdot y ~t ~x =
+let xdot ~t ~x ~y =
   y
 
-let ydot x ~t ~x:y =
-  ~-.x
+let ydot ~t ~x ~y =
+  -.y
 
 let rec loop i n dt x_euler y_euler x_rk4 y_rk4 =
   if i > n then
     ()
   else begin
     let t = float i *. dt in
-    let x_euler' = Orddiff.euler ~dt ~t ~x:x_euler ~xdot:(xdot y_euler) in
-    let y_euler' = Orddiff.euler ~dt ~t ~x:y_euler ~xdot:(ydot x_euler) in
-    let x_rk4' = Orddiff.rk4 ~dt ~t ~x:x_rk4 ~xdot:(xdot y_rk4) in
-    let y_rk4' = Orddiff.rk4 ~dt ~t ~x:y_rk4 ~xdot:(ydot x_rk4) in
+    let x_euler', y_euler' = Orddiff.euler_2 ~dt ~t ~x:x_euler ~y:y_euler ~xdot ~ydot in
+    let x_rk4', y_rk4' = Orddiff.rk4_2 ~dt ~t ~x:x_rk4 ~y:y_rk4 ~xdot ~ydot in
     printf "%g\t%g\t%g\n" t x_euler' x_rk4';
     loop (i + 1) n dt x_euler' y_euler' x_rk4' y_rk4'
   end
 
 let () =
-  let n = 100 in
-  let dt = 10.0 /. float n in
+  let n = 100000 in
+  let dt = 0.0001 in
   let x0 = 1.0 in
-  let y0 = 0.0 in
+  let y0 = -1.0 in
   loop 1 n dt x0 y0 x0 y0
--- orddiff.ml
+++ orddiff.ml
@@ -2,8 +2,28 @@ let euler ~dt ~t ~x ~xdot =
   x +. (xdot ~t ~x) *. dt
 
 let rk4 ~dt ~t ~x ~xdot =
-  let k1 = xdot ~t ~x in
-  let k2 = xdot ~t:(t +. dt /. 2.0) ~x:(x +. dt /. 2.0 *. k1) in
-  let k3 = xdot ~t:(t +. dt /. 2.0) ~x:(x +. dt /. 2.0 *. k2) in
-  let k4 = xdot ~t:(t +. dt) ~x:(x +. dt *. k3)  in
-  x +. dt /. 6.0 *. (k1 +. 2.0 *. k2 +. 2.0 *. k3 +. k4)
+  let dt2 = dt /. 2.0 in
+  let f1 = xdot ~t ~x in
+  let f2 = xdot ~t:(t +. dt2) ~x:(x +. dt2 *. f1) in
+  let f3 = xdot ~t:(t +. dt2) ~x:(x +. dt2 *. f2) in
+  let f4 = xdot ~t:(t +. dt) ~x:(x +. dt *. f3) in
+  x +. dt /. 6.0 *. (f1 +. 2.0 *. f2 +. 2.0 *. f3 +. f4)
+
+let euler_2 ~dt ~t ~x ~y ~xdot ~ydot =
+  let x' = x +. (xdot ~t ~x ~y) *. dt in
+  let y' = y +. (ydot ~t ~x ~y) *. dt in
+  x', y'
+
+let rk4_2 ~dt ~t ~x ~y ~xdot ~ydot =
+  let dt2 = dt /. 2.0 in
+  let f1 = xdot ~t ~x ~y in
+  let g1 = ydot ~t ~x ~y in
+  let f2 = xdot ~t:(t +. dt2) ~x:(x +. dt2 *. f1) ~y:(y +. dt2 *. g1) in
+  let g2 = ydot ~t:(t +. dt2) ~x:(x +. dt2 *. f1) ~y:(y +. dt2 *. g1) in
+  let f3 = xdot ~t:(t +. dt2) ~x:(x +. dt2 *. f2) ~y:(y +. dt2 *. g2) in
+  let g3 = ydot ~t:(t +. dt2) ~x:(x +. dt2 *. f2) ~y:(y +. dt2 *. g2) in
+  let f4 = xdot ~t:(t +. dt) ~x:(x +. dt *. f3) ~y:(y +. dt *. g3) in
+  let g4 = ydot ~t:(t +. dt) ~x:(x +. dt *. f3) ~y:(y +. dt *. g3) in
+  let x' = x +. dt /. 6.0 *. (f1 +. 2.0 *. f2 +. 2.0 *. f3 +. f4) in
+  let y' = y +. dt /. 6.0 *. (g1 +. 2.0 *. g2 +. 2.0 *. g3 +. g4) in
+  x', y'
