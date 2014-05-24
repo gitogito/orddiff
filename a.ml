9508479176c0d472fc6a51ac517a7f0c6601d9cc
Yoshiki Wada <yoshiki.wada@gmail.com>
2014-05-24 20:28:14 +0900

    Use d2x/dt2 = -x

--- a.ml
+++ a.ml
@@ -1,19 +1,27 @@
 open Printf
 
-let xdot ~t ~x =
-  x
+let xdot y ~t ~x =
+  y
 
-let rec loop i n dt x_euler x_rk4 =
+let ydot x ~t ~x:y =
+  ~-.x
+
+let rec loop i n dt x_euler y_euler x_rk4 y_rk4 =
   if i > n then
     ()
   else begin
     let t = float i *. dt in
-    let x_euler' = Orddiff.euler ~dt ~t ~x:x_euler ~xdot in
-    let x_rk4'   = Orddiff.rk4   ~dt ~t ~x:x_rk4   ~xdot in
+    let x_euler' = Orddiff.euler ~dt ~t ~x:x_euler ~xdot:(xdot y_euler) in
+    let y_euler' = Orddiff.euler ~dt ~t ~x:y_euler ~xdot:(ydot x_euler) in
+    let x_rk4' = Orddiff.rk4 ~dt ~t ~x:x_rk4 ~xdot:(xdot y_rk4) in
+    let y_rk4' = Orddiff.rk4 ~dt ~t ~x:y_rk4 ~xdot:(ydot x_rk4) in
     printf "%g\t%g\t%g\n" t x_euler' x_rk4';
-    loop (i + 1) n dt x_euler' x_rk4'
+    loop (i + 1) n dt x_euler' y_euler' x_rk4' y_rk4'
   end
 
 let () =
   let n = 100 in
-  loop 1 n (1.0 /. float n) 1.0 1.0
+  let dt = 10.0 /. float n in
+  let x0 = 1.0 in
+  let y0 = 0.0 in
+  loop 1 n dt x0 y0 x0 y0
