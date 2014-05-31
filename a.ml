d5cc11b616022d065531d4ee1e002654ca0a9cfd
Yoshiki Wada <yoshiki.wada@gmail.com>
2014-05-31 23:24:45 +0900

    Use modules
    - Euler
    - Rk4

--- a.ml
+++ a.ml
@@ -1,31 +1,33 @@
 open Printf
 open ExtLib
 
-let xdot ~t ~x ~y =
-  y
-
-let ydot ~t ~x ~y =
-  -.x
-
 let xdot_ary = [|
   (fun ~t ~x_ary -> x_ary.(1));
   (fun ~t ~x_ary -> -.x_ary.(0));
 |]
 
-let rec loop i n dt x_ary_euler x_ary_rk4 =
+let rec loop i n dt euler rk4 =
   if i > n then
-    ()
+    (euler.Orddiff.Euler.get_x_ary (), rk4.Orddiff.Rk4.get_x_ary ())
   else begin
     let t = float i *. dt in
-    let x_ary_euler = Orddiff.euler_x ~dt ~t ~x_ary:x_ary_euler ~xdot_ary in
-    let x_ary_rk4 = Orddiff.rk4_x ~dt ~t ~x_ary:x_ary_rk4 ~xdot_ary in
-    printf "%g\t%g\t%g\n" t x_ary_euler.(0) x_ary_rk4.(0);
-    loop (i + 1) n dt x_ary_euler x_ary_rk4
+    let x_ary_euler = euler.Orddiff.Euler.update () in
+    let x_ary_rk4 = rk4.Orddiff.Rk4.update () in
+  (*
+    printf "%g\t%g\t%g\n"
+      t
+      (euler.Orddiff.Euler.get_x_ary ()).(0)
+      (rk4.Orddiff.Rk4.get_x_ary ()).(0);
+   *)
+    loop (i + 1) n dt euler rk4
   end
 
 let () =
-  let n = 100000 in
+  let n = 10000000 in
   let dt = 0.01 in
   let x_ary_euler = [| 1.0; 0.0 |] in
+  let euler = Orddiff.Euler.init ~dt ~x_ary:x_ary_euler ~xdot_ary in
   let x_ary_rk4 = [| 1.0; 0.0 |] in
-  loop 1 n dt x_ary_euler x_ary_rk4
+  let rk4 = Orddiff.Rk4.init ~dt ~x_ary:x_ary_rk4 ~xdot_ary in
+  let x_ary_euler, x_ary_rk4 = loop 1 n dt euler rk4 in
+  printf "%g\t%g\n" x_ary_euler.(0) x_ary_rk4.(0)
--- orddiff.ml
+++ orddiff.ml
@@ -1,74 +1,97 @@
-let euler ~dt ~t ~x ~xdot =
-  x +. (xdot ~t ~x) *. dt
+module Base = struct
+  type t = {
+    update : unit -> unit;
+    get_x_ary : unit -> float array;
+  }
+end
 
-let rk4 ~dt ~t ~x ~xdot =
-  let dt2 = dt /. 2.0 in
-  let f1 = xdot ~t ~x in
-  let f2 = xdot ~t:(t +. dt2) ~x:(x +. dt2 *. f1) in
-  let f3 = xdot ~t:(t +. dt2) ~x:(x +. dt2 *. f2) in
-  let f4 = xdot ~t:(t +. dt) ~x:(x +. dt *. f3) in
-  x +. dt /. 6.0 *. (f1 +. 2.0 *. f2 +. 2.0 *. f3 +. f4)
+module Euler = struct
+  include Base
 
-let euler_2 ~dt ~t ~x ~y ~xdot ~ydot =
-  let x' = x +. (xdot ~t ~x ~y) *. dt in
-  let y' = y +. (ydot ~t ~x ~y) *. dt in
-  x', y'
+  type t' = {
+    dt : float;
+    mutable t : float;
+    mutable x_ary : float array;
+    xdot_ary : (t:float -> x_ary:float array -> float) array;
+  }
 
-let rk4_2 ~dt ~t ~x ~y ~xdot ~ydot =
-  let dt2 = dt /. 2.0 in
-  let f1 = xdot ~t ~x ~y in
-  let g1 = ydot ~t ~x ~y in
-  let f2 = xdot ~t:(t +. dt2) ~x:(x +. dt2 *. f1) ~y:(y +. dt2 *. g1) in
-  let g2 = ydot ~t:(t +. dt2) ~x:(x +. dt2 *. f1) ~y:(y +. dt2 *. g1) in
-  let f3 = xdot ~t:(t +. dt2) ~x:(x +. dt2 *. f2) ~y:(y +. dt2 *. g2) in
-  let g3 = ydot ~t:(t +. dt2) ~x:(x +. dt2 *. f2) ~y:(y +. dt2 *. g2) in
-  let f4 = xdot ~t:(t +. dt) ~x:(x +. dt *. f3) ~y:(y +. dt *. g3) in
-  let g4 = ydot ~t:(t +. dt) ~x:(x +. dt *. f3) ~y:(y +. dt *. g3) in
-  let x' = x +. dt /. 6.0 *. (f1 +. 2.0 *. f2 +. 2.0 *. f3 +. f4) in
-  let y' = y +. dt /. 6.0 *. (g1 +. 2.0 *. g2 +. 2.0 *. g3 +. g4) in
-  x', y'
+  let update self () =
+    self.t <- self.t +. self.dt;
+    self.x_ary <- Array.mapi
+                    (fun i x -> x +. (self.xdot_ary.(i) ~t:self.t ~x_ary:self.x_ary) *. self.dt)
+                    self.x_ary
 
-let euler_x ~dt ~t ~x_ary ~xdot_ary =
-  let order = Array.length x_ary in
-  let f_ary = Array.make order 0.0 in
-  for i = 0 to order - 1 do
-    f_ary.(i) <- x_ary.(i) +. (xdot_ary.(i) ~t ~x_ary) *. dt
-  done;
-  f_ary
+  let get_x_ary (self : t') () =
+    self.x_ary
 
-let rk4_x ~dt ~t ~x_ary ~xdot_ary =
-  let order = Array.length x_ary in
-  let f1_ary = Array.make order 0.0 in
-  let f2_ary = Array.make order 0.0 in
-  let f3_ary = Array.make order 0.0 in
-  let f4_ary = Array.make order 0.0 in
-  let dt2 = dt /. 2.0 in
+  let init ~dt ~x_ary ~xdot_ary =
+    let self = {
+      dt;
+      t = 0.0;
+      x_ary;
+      xdot_ary;
+    } in
+    { update = update self;
+      get_x_ary = get_x_ary self; }
+end
 
-  for i = 0 to order - 1 do
-    f1_ary.(i) <- xdot_ary.(i) ~t ~x_ary
-  done;
+module Rk4 = struct
+  include Base
 
-  let x_ary' = Array.mapi (fun i x -> x +. dt2 *. f1_ary.(i)) x_ary in
-  for i = 0 to order - 1 do
-    f2_ary.(i) <- xdot_ary.(i) ~t:(t +. dt2) ~x_ary:x_ary'
-  done;
+  type t' = {
+    dt : float;
+    mutable t : float;
+    mutable x_ary : float array;
+    xdot_ary : (t:float -> x_ary:float array -> float) array;
+  }
 
-  let x_ary' = Array.mapi (fun i x -> x +. dt2 *. f2_ary.(i)) x_ary in
-  for i = 0 to order - 1 do
-    f3_ary.(i) <- xdot_ary.(i) ~t:(t +. dt2) ~x_ary:x_ary'
-  done;
+  let update self () =
+    self.t <- self.t +. self.dt;
 
-  let x_ary' = Array.mapi (fun i x -> x +. dt *. f3_ary.(i)) x_ary in
-  for i = 0 to order - 1 do
-    f4_ary.(i) <- xdot_ary.(i) ~t:(t +. dt) ~x_ary:x_ary'
-  done;
+    let order = Array.length self.x_ary in
+    let f1_ary = Array.make order 0.0 in
+    let f2_ary = Array.make order 0.0 in
+    let f3_ary = Array.make order 0.0 in
+    let f4_ary = Array.make order 0.0 in
+    let dt2 = self.dt /. 2.0 in
 
-  for i = 0 to order - 1 do
-    let f1 = f1_ary.(i) in
-    let f2 = f2_ary.(i) in
-    let f3 = f3_ary.(i) in
-    let f4 = f4_ary.(i) in
-    x_ary.(i) <- x_ary.(i) +. dt /. 6.0 *. (f1 +. 2.0 *. f2 +. 2.0 *. f3 +. f4)
-  done;
+    for i = 0 to order - 1 do
+      f1_ary.(i) <- self.xdot_ary.(i) ~t:self.t ~x_ary:self.x_ary
+    done;
 
-  x_ary
+    let x_ary' = Array.mapi (fun i x -> x +. dt2 *. f1_ary.(i)) self.x_ary in
+    for i = 0 to order - 1 do
+      f2_ary.(i) <- self.xdot_ary.(i) ~t:(self.t +. dt2) ~x_ary:x_ary'
+    done;
+
+    let x_ary' = Array.mapi (fun i x -> x +. dt2 *. f2_ary.(i)) self.x_ary in
+    for i = 0 to order - 1 do
+      f3_ary.(i) <- self.xdot_ary.(i) ~t:(self.t +. dt2) ~x_ary:x_ary'
+    done;
+
+    let x_ary' = Array.mapi (fun i x -> x +. self.dt *. f3_ary.(i)) self.x_ary in
+    for i = 0 to order - 1 do
+      f4_ary.(i) <- self.xdot_ary.(i) ~t:(self.t +. self.dt) ~x_ary:x_ary'
+    done;
+
+    for i = 0 to order - 1 do
+      let f1 = f1_ary.(i) in
+      let f2 = f2_ary.(i) in
+      let f3 = f3_ary.(i) in
+      let f4 = f4_ary.(i) in
+      self.x_ary.(i) <- self.x_ary.(i) +. self.dt /. 6.0 *. (f1 +. 2.0 *. f2 +. 2.0 *. f3 +. f4)
+    done
+
+  let get_x_ary (self : t') () =
+    self.x_ary
+
+  let init ~dt ~x_ary ~xdot_ary =
+    let self = {
+      dt;
+      t = 0.0;
+      x_ary;
+      xdot_ary;
+    } in
+    { update = update self;
+      get_x_ary = get_x_ary self; }
+end
