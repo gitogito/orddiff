ade6e92d8eb47104bb5b8be3451f661c9f8fda80
Yoshiki Wada <yoshiki.wada@gmail.com>
2014-05-31 22:36:35 +0900

    Add *_x
    - euler_x
    - rk4_x

--- a.ml
+++ a.ml
@@ -1,25 +1,31 @@
 open Printf
+open ExtLib
 
 let xdot ~t ~x ~y =
   y
 
 let ydot ~t ~x ~y =
-  -.y
+  -.x
 
-let rec loop i n dt x_euler y_euler x_rk4 y_rk4 =
+let xdot_ary = [|
+  (fun ~t ~x_ary -> x_ary.(1));
+  (fun ~t ~x_ary -> -.x_ary.(0));
+|]
+
+let rec loop i n dt x_ary_euler x_ary_rk4 =
   if i > n then
     ()
   else begin
     let t = float i *. dt in
-    let x_euler', y_euler' = Orddiff.euler_2 ~dt ~t ~x:x_euler ~y:y_euler ~xdot ~ydot in
-    let x_rk4', y_rk4' = Orddiff.rk4_2 ~dt ~t ~x:x_rk4 ~y:y_rk4 ~xdot ~ydot in
-    printf "%g\t%g\t%g\n" t x_euler' x_rk4';
-    loop (i + 1) n dt x_euler' y_euler' x_rk4' y_rk4'
+    let x_ary_euler = Orddiff.euler_x ~dt ~t ~x_ary:x_ary_euler ~xdot_ary in
+    let x_ary_rk4 = Orddiff.rk4_x ~dt ~t ~x_ary:x_ary_rk4 ~xdot_ary in
+    printf "%g\t%g\t%g\n" t x_ary_euler.(0) x_ary_rk4.(0);
+    loop (i + 1) n dt x_ary_euler x_ary_rk4
   end
 
 let () =
   let n = 100000 in
-  let dt = 0.0001 in
-  let x0 = 1.0 in
-  let y0 = -1.0 in
-  loop 1 n dt x0 y0 x0 y0
+  let dt = 0.01 in
+  let x_ary_euler = [| 1.0; 0.0 |] in
+  let x_ary_rk4 = [| 1.0; 0.0 |] in
+  loop 1 n dt x_ary_euler x_ary_rk4
--- orddiff.ml
+++ orddiff.ml
@@ -27,3 +27,48 @@ let rk4_2 ~dt ~t ~x ~y ~xdot ~ydot =
   let x' = x +. dt /. 6.0 *. (f1 +. 2.0 *. f2 +. 2.0 *. f3 +. f4) in
   let y' = y +. dt /. 6.0 *. (g1 +. 2.0 *. g2 +. 2.0 *. g3 +. g4) in
   x', y'
+
+let euler_x ~dt ~t ~x_ary ~xdot_ary =
+  let order = Array.length x_ary in
+  let f_ary = Array.make order 0.0 in
+  for i = 0 to order - 1 do
+    f_ary.(i) <- x_ary.(i) +. (xdot_ary.(i) ~t ~x_ary) *. dt
+  done;
+  f_ary
+
+let rk4_x ~dt ~t ~x_ary ~xdot_ary =
+  let order = Array.length x_ary in
+  let f1_ary = Array.make order 0.0 in
+  let f2_ary = Array.make order 0.0 in
+  let f3_ary = Array.make order 0.0 in
+  let f4_ary = Array.make order 0.0 in
+  let dt2 = dt /. 2.0 in
+
+  for i = 0 to order - 1 do
+    f1_ary.(i) <- xdot_ary.(i) ~t ~x_ary
+  done;
+
+  let x_ary' = Array.mapi (fun i x -> x +. dt2 *. f1_ary.(i)) x_ary in
+  for i = 0 to order - 1 do
+    f2_ary.(i) <- xdot_ary.(i) ~t:(t +. dt2) ~x_ary:x_ary'
+  done;
+
+  let x_ary' = Array.mapi (fun i x -> x +. dt2 *. f2_ary.(i)) x_ary in
+  for i = 0 to order - 1 do
+    f3_ary.(i) <- xdot_ary.(i) ~t:(t +. dt2) ~x_ary:x_ary'
+  done;
+
+  let x_ary' = Array.mapi (fun i x -> x +. dt *. f3_ary.(i)) x_ary in
+  for i = 0 to order - 1 do
+    f4_ary.(i) <- xdot_ary.(i) ~t:(t +. dt) ~x_ary:x_ary'
+  done;
+
+  for i = 0 to order - 1 do
+    let f1 = f1_ary.(i) in
+    let f2 = f2_ary.(i) in
+    let f3 = f3_ary.(i) in
+    let f4 = f4_ary.(i) in
+    x_ary.(i) <- x_ary.(i) +. dt /. 6.0 *. (f1 +. 2.0 *. f2 +. 2.0 *. f3 +. f4)
+  done;
+
+  x_ary
