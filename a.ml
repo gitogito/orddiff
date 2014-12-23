683f645524b9415d430a38f871a3357feae523e7
Yoshiki Wada <yoshiki.wada@gmail.com>
2014-12-23 17:40:39 +0900

    change:
    * change a differential equation to "y y' = x".
    * change to use "t - 1" instead of "t".

--- a.ml
+++ a.ml
@@ -1,19 +1,25 @@
 open Printf
 
 let xdot ~t ~x =
-  x
+  t /. x
 
 let rec loop i n dt x_euler x_rk4 =
   if i > n then
     ()
   else begin
+    let x_euler', x_rk4' =
+      if i = 0 then
+        (x_euler, x_rk4)
+      else begin
+        let t = float (i - 1) *. dt in
+        (Orddiff.euler ~dt ~t ~x:x_euler ~xdot, Orddiff.rk4 ~dt ~t ~x:x_rk4 ~xdot)
+      end
+    in
     let t = float i *. dt in
-    let x_euler' = Orddiff.euler ~dt ~t ~x:x_euler ~xdot in
-    let x_rk4'   = Orddiff.rk4   ~dt ~t ~x:x_rk4   ~xdot in
     printf "%g\t%g\t%g\n" t x_euler' x_rk4';
     loop (i + 1) n dt x_euler' x_rk4'
   end
 
 let () =
-  let n = 100 in
-  loop 1 n (1.0 /. float n) 1.0 1.0
+  let n = 10 in
+  loop 0 n (1.0 /. float n) 1.0 1.0
