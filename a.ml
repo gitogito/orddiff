0687c211af2b51054b2128571865b1ac7013aef9
Yoshiki Wada <yoshiki.wada@gmail.com>
2014-12-23 18:03:15 +0900

    beautify code

--- a.ml
+++ a.ml
@@ -6,26 +6,30 @@ let xdot_ary = [|
   (fun ~t ~x_ary -> ignore t; -.x_ary.(0));
 |]
 
-let rec loop i n dt euler rk4 =
-  if i > n then
-    (euler.Orddiff.Euler.get_x_ary (), rk4.Orddiff.Rk4.get_x_ary ())
-  else begin
-    let t = float i *. dt in
-    euler.Orddiff.Euler.update ();
-    rk4.Orddiff.Rk4.update ();
-    printf "%g\t%g\t%g\n"
-      t
+let loop n dt euler rk4 =
+  let print t x_euler x_rk4 =
+    printf "%g\t%g\t%g\n" t x_euler x_rk4
+  in
+  let rec aux i n dt euler rk4 =
+    print
+      (float i *. dt)
       (euler.Orddiff.Euler.get_x_ary ()).(0)
       (rk4.Orddiff.Rk4.get_x_ary ()).(0);
-    loop (i + 1) n dt euler rk4
-  end
+    if i = n then
+      ()
+    else begin
+      euler.Orddiff.Euler.update ();
+      rk4.Orddiff.Rk4.update ();
+      aux (i + 1) n dt euler rk4
+    end
+  in
+  aux 0 n dt euler rk4
 
 let () =
-  let n = 1000 in
-  let dt = 0.01 in
+  let n = 100 in
+  let dt = 0.1 in
   let x_ary_euler = [| 1.0; 0.0 |] in
   let euler = Orddiff.Euler.init ~dt ~x_ary:x_ary_euler ~xdot_ary in
   let x_ary_rk4 = [| 1.0; 0.0 |] in
   let rk4 = Orddiff.Rk4.init ~dt ~x_ary:x_ary_rk4 ~xdot_ary in
-  let x_ary_euler, x_ary_rk4 = loop 1 n dt euler rk4 in
-  printf "#%g\t%g\n" x_ary_euler.(0) x_ary_rk4.(0)
+  loop n dt euler rk4
