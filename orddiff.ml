46f96cd6eb51b347c648326e6f278f60b3e41743
Yoshiki Wada <yoshiki.wada@gmail.com>
2015-01-10 23:36:14 +0900

    remove order from record

--- orddiff.ml
+++ orddiff.ml
@@ -13,16 +13,16 @@ module Euler = struct
     dxdt : float -> float array -> float array -> unit;
     xs : float array;
     mutable n : int;
-    order : int;
     fs : float array;
   }
 
   let update self () =
     let t = self.dt *. float self.n in
     self.dxdt t self.xs self.fs;
-    for i = 0 to self.order - 1 do
-      self.xs.(i) <- self.xs.(i) +. self.fs.(i) *. self.dt
-    done;
+    Array.iteri
+      (fun i x ->
+         self.xs.(i) <- x +. self.fs.(i) *. self.dt)
+      self.xs;
     self.n <- self.n + 1
 
   let get_t self () =
@@ -35,7 +35,6 @@ module Euler = struct
       dxdt;
       xs;
       n = 0;
-      order;
       fs = Array.make order 0.0;
     } in
     {
@@ -52,7 +51,6 @@ module Rk4 = struct
     dxdt : float -> float array -> float array -> unit;
     xs : float array;
     mutable n : int;
-    order : int;
     f1s : float array;
     f2s : float array;
     f3s : float array;
@@ -69,14 +67,14 @@ module Rk4 = struct
     self.dxdt (t +. self.dt2) xs' self.f3s;
     let xs' = Array.mapi (fun i x -> x +. self.dt *. self.f3s.(i)) self.xs in
     self.dxdt (t +. self.dt) xs' self.f4s;
-
-    for i = 0 to self.order - 1 do
-      let f1 = self.f1s.(i) in
-      let f2 = self.f2s.(i) in
-      let f3 = self.f3s.(i) in
-      let f4 = self.f4s.(i) in
-      self.xs.(i) <- self.xs.(i) +. self.dt /. 6.0 *. (f1 +. 2.0 *. f2 +. 2.0 *. f3 +. f4)
-    done;
+    Array.iteri
+      (fun i x ->
+         let f1 = self.f1s.(i) in
+         let f2 = self.f2s.(i) in
+         let f3 = self.f3s.(i) in
+         let f4 = self.f4s.(i) in
+         self.xs.(i) <- x +. self.dt /. 6.0 *. (f1 +. 2.0 *. f2 +. 2.0 *. f3 +. f4))
+      self.xs;
     self.n <- self.n + 1
 
   let get_t self () =
@@ -89,7 +87,6 @@ module Rk4 = struct
       dxdt;
       xs;
       n = 0;
-      order;
       f1s = Array.make order 0.0;
       f2s = Array.make order 0.0;
       f3s = Array.make order 0.0;
